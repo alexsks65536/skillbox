b8623d48089ce19ef1bd8a946968a08c8d2b6c6b
alexsks65536 <megbyte@mail.ru>
2022-09-29 22:43:22 +0300

    parsers

--- parser2.py
+++ parser2.py
@@ -4,6 +4,7 @@ htmlfile = open("skillbox.html", "r", encoding="utf-8")
 code = htmlfile.read()
 soup = BeautifulSoup(code)
 links = soup.findAll("a")  # селектор "а" который найдет все теги ссылок <a></>
+
 print([link.string.strip() for link in links])
 
 
--- parser3.py
+++ parser3.py
@@ -20,7 +20,6 @@ webinars = skillsoup.findAll(class_="webinar-card__title")
 
 # пробуем получить дату и название
 # webinars__item
-
 webinars_full = skillsoup.findAll(class_="webinars__item")
 for webinar in webinars_full:
     title = webinar.find(class_="webinar-card__title").string.strip()
